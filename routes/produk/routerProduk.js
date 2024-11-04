046117751e085d591a485a936cd407a00ac7b419
rifqyaliansyah <rifqyaliansyah1877@gmail.com>
2024-11-04 11:57:02 +0700

    update

--- routes/produk/routerProduk.js
+++ routes/produk/routerProduk.js
@@ -5,7 +5,7 @@ const protect = require('../../middelware/auth')
 
 // Rute produk
 router.get('/getProduk/:id', getProdukById);
-router.post('/createProduk', upload.single('image'), createProduk);
+router.post('/createProduk', protect(['kasir']), upload.single('image'), createProduk);
 router.patch('/updateProduk/:id', upload.single('image'), updateProduk);
 router.delete('/deleteProduk/:id', deleteProduk);
 
